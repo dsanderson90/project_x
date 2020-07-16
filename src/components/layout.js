099554c9417e5e48c5d70b247b9710270c4555f2
David J. Sanderson <sanderdj90@gmail.com>
2020-07-16 17:32:56 -0600

    feat: RadioNav

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-
+import { Container, Flex } from "theme-ui"
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -19,24 +19,24 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <Container
+    py={4}
+    px={8}
+    bg='muted'
+    Centered container
+    >
+
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
-      \
-    </>
+
+      
+    </Container>
   )
 }
 
