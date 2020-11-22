b43fe531ba4fa94e212d8de22aaba18800d6663e
David <sanderdj90@gmail.com>
2020-11-22 14:04:08 -0700

    fix casing

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import Helmet from "react-helmet"
+export default function Header({ children, siteTitle, description }) {
+  return (
+    <header>
+      <Helmet htmlAttributes={{ lang : 'en' }}>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <title>{siteTitle}</title>
+        <link rel="canonical" href="https://davidsanderson.dev" />
+      </Helmet>
+      {children}
+    </header>
+  )
+}
