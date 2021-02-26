897725e98436ca60d7ec579fb8d2fb80ac491196
David Sanderson <43357044+d-sanderson@users.noreply.github.com>
2021-02-25 21:57:44 -0700

    Delete src/components/Sections directory

--- src/components/Sections/index.js
+++
@@ -1,5 +0,0 @@
-export { default as About } from "./About"
-export { default as Blog } from "./Blog"
-export { default as Projects } from "./Projects"
-export { default as Resume } from "./Resume"
-export { default as Snippets } from "./Snippets"
